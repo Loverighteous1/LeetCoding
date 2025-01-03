b6abb4d7d8488794d357518171df7afc19c353b3
Loverighteous1 <dsenanou@aimsammi.org>
2025-01-03 13:41:07 +0000

    Pascal's Triangle. Take an integer and retrun a list of list of length integer.

--- PascalsTriangle.py
+++ PascalsTriangle.py
@@ -13,7 +13,7 @@ Constraints:
 1 <= numRows <= 30
  
 """
-
+from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
@@ -24,3 +24,31 @@ class Solution:
         Input:  numRows -> an integer
         Return: The first numRows of Pascal's triangle.
         """
+        output = [[1], [1,1]]
+        
+        assert 1 <= numRows <= 30, f"Integer must be in [1, 30]"
+
+        if numRows == 1:
+            print(output[0])
+        elif numRows == 2:
+            print(output)
+        else:
+            for i in range(1, numRows-1):
+                previous_l = output[i]
+                len_l = len(previous_l)
+                newlist = [1]
+                for j in range(1, len_l):
+                    #print(j)
+                    newlist.append(previous_l[j] + previous_l[j-1])
+                newlist.append(1)
+                #print(newlist)
+                output.append(newlist)
+            print(output)
+            
+            #Other form of priting
+
+            # for i in range(len(output)):
+            #     print(output[i],'\n')
+
+solution = Solution()
+solution.generate(5)
