61ab9443c4d8530f7e293bcc2e89d9b98c1c1bbc
Loverighteous1 <dsenanou@aimsammi.org>
2025-03-09 17:55:46 +0000

    Return the dataframe of the id of the temperatures that are greater than the previous date temperature

---
+++ RisingTemperature.py
@@ -0,0 +1,60 @@
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' id' with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+import pandas as pd
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    new_id = []
+    id_ = weather.id
+    temp_ = weather.temperature
+    print(type(id_))
+    print(type(temp_))
+    for ind in range(len(temp_)-1):
+    if temp_[ind] < temp_[ind+1]:
+        new_id.append(id_[ind + 1])
+
+    new_id = pd.DataFrame({'id':new_id})
+    return new_id
