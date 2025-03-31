81fa2019f3ff03481d3bd45a466aac6b4cd6c80e
Loverighteous1 <dsenanou@aimsammi.org>
2025-03-31 01:24:57 +0000

    Rising Temperature

--- RisingTemperature.py
+++ RisingTemperature.py
@@ -8,8 +8,8 @@ def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
     print(type(id_))
     print(type(temp_))
     for ind in range(len(temp_)-1):
-    if temp_[ind] < temp_[ind+1]:
-        new_id.append(id_[ind + 1])
+        if temp_[ind] < temp_[ind+1]:
+            new_id.append(id_[ind + 1])
 
     new_id = pd.DataFrame({'id':new_id})
     return new_id
